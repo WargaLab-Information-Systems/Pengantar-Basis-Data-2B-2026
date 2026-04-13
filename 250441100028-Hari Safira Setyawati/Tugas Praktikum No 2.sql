5b55cb46d43e5b86d1bab34a49ad54118b2f309a
Hari Safira Setyawati <harisafirasetyawati@gmail.com>
2026-04-13 18:50:12 +0700

    250441100028_Hari Safira Setyawati_Modul2_Asprak Kak Faisal

---
+++ 250441100028-Hari Safira Setyawati/Tugas Praktikum No 2.sql
@@ -0,0 +1,16 @@
+INSERT INTO dokter VALUES
+(1, 'Dr. Anang', 'Mata'),
+(2, 'Dr. Ani', 'Anak');
+
+INSERT INTO pasien VALUES
+(1, 'Salman', '2007-07-07', '08123456789'),
+(2, 'Rizky', '2008-08-08', '08987654321');
+
+INSERT INTO rekam_medis VALUES
+(1,1,1, '2026-06-26', 'Mata Kunang-Kunang'),
+(2,2,2, '2026-01-01', 'Demam Tinggi');
+
+INSERT INTO rekam_medis VALUES
+(3,3,3, '2026-01-01', 'Sakit Gigi');
+
+SELECT * FROM dokter;
